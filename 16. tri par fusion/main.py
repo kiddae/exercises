ba9349f7180b8692c8aaa28156cbd1e4d138897f
Louise Nguyen <nguyenlelouise@gmail.com>
2022-04-28 21:19:54 +0200

    change 14

--- 16. tri par fusion/main.py
+++ 16. tri par fusion/main.py
@@ -5,12 +5,12 @@ def fusion(A, p, q, r):
     R = [A[q+1+i] for i in range(n2)] + [float('inf')]
     i = 0
     j = 0
-    for k in range(r-p+1):
+    for k in range(p, r+1):
         if L[i] <= R[j]:
-            A[p+k] = L[i]
+            A[k] = L[i]
             i += 1
         else:
-            A[p+k] = R[j]
+            A[k] = R[j]
             j += 1
 
 
@@ -24,7 +24,7 @@ def tri_fusion(A, p, r):
 
 if __name__ == '__main__':
     from random import randint
-    a = [randint(1, 10) for _ in range(4)]
+    a = [randint(1, 10) for _ in range(100)]
     print('Tableau initial:')
     print(a)
 
