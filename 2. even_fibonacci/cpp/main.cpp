809d2b185e8df34e4934fc63b0e39baebb133d9e
Louise Nguyen <nguyenlelouise@gmail.com>
2022-05-26 17:37:59 +0200

    quick changes (format) to old exercices

--- 2. even_fibonacci/cpp/main.cpp
+++ 2. even_fibonacci/cpp/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int even_fib(int n)
-{
-    int* fib = new int[n];
-    fib[0] = 0, fib[1] = 1;
-    int sum = 0;
-    for (int i=2; i<n; i++)
-    {
-        fib[i] = fib[i-1] + fib[i-2];
-        sum += (fib[i] % 2 == 0) ? fib[i] : 0;
-    }
-    delete [] fib;
-    return sum;
+int even_fib(int n) {
+  int *fib = new int[n];
+  fib[0] = 0, fib[1] = 1;
+  int sum = 0;
+  for (int i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+    sum += (fib[i] % 2 == 0) ? fib[i] : 0;
+  }
+  delete[] fib;
+  return sum;
 }
 
-int main()
-{
-    cout << "What limit? ";
-    int a;
-    cin >> a;
-    cout << "Sum of all even values of the Fibonacci sequence up to " << a << ": " << even_fib(a) << endl;
+int main() {
+  cout << "What limit? ";
+  int a;
+  cin >> a;
+  cout << "Sum of all even values of the Fibonacci sequence up to " << a << ": "
+       << even_fib(a) << endl;
 }
--- 3. cpp_min_max/main.cpp
+++ 3. cpp_min_max/main.cpp
@@ -1,34 +1,32 @@
-#include <iostream>
 #include <climits>
-using std::cout;
+#include <iostream>
 using std::cin;
+using std::cout;
 using std::endl;
 
-int main()
-{
-    const int TABLEN = 10;
-    int tab[TABLEN];
-    cout << "Insert " << TABLEN << " elements:" << endl;
-    for (int i=0; i<TABLEN; i++) cin >> tab[i];
+int main() {
+  const int TABLEN = 10;
+  int tab[TABLEN];
+  cout << "Insert " << TABLEN << " elements:" << endl;
+  for (int i = 0; i < TABLEN; i++)
+    cin >> tab[i];
 
-    int maximum = -INT_MAX;
-    int minimum = INT_MAX;
+  int maximum = -INT_MAX;
+  int minimum = INT_MAX;
 
-    // USING ARRAY ELEMENTS
-    /* for (int i=0; i<TABLEN; i++)
-    {
-        maximum = (tab[i] > maximum) ? tab[i] : maximum;
-        minimum = (tab[i] < minimum) ? tab[i] : minimum;
-    }*/
+  // USING ARRAY ELEMENTS
+  /* for (int i=0; i<TABLEN; i++)
+  {
+      maximum = (tab[i] > maximum) ? tab[i] : maximum;
+      minimum = (tab[i] < minimum) ? tab[i] : minimum;
+  }*/
 
-    // USING POINTER PROPERTIES 
-    for (int i=0; i<TABLEN; i++)
-    {
-        maximum = (*(tab+i) > maximum) ? *(tab+i) : maximum;
-        minimum = (*(tab+i) < minimum) ? *(tab+i) : minimum;
-    }
+  // USING POINTER PROPERTIES
+  for (int i = 0; i < TABLEN; i++) {
+    maximum = (*(tab + i) > maximum) ? *(tab + i) : maximum;
+    minimum = (*(tab + i) < minimum) ? *(tab + i) : minimum;
+  }
 
-    cout << "Maximum is " << maximum << endl;
-    cout << "Minimum is " << minimum << endl;
+  cout << "Maximum is " << maximum << endl;
+  cout << "Minimum is " << minimum << endl;
 }
-
